c7bebb5711683c29db15f40ed368a55de8e15edd
ChiniGumy <dsebastiansr@hotmail.com>
2022-12-19 11:54:39 -0500

    [ADD] Timer added

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,11 +22,17 @@ Partial Class Form1
 	'No lo modifique con el editor de código.
 	<System.Diagnostics.DebuggerStepThrough()>
 	Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.radioButtonEasy = New System.Windows.Forms.RadioButton()
         Me.radioButtonIntermedian = New System.Windows.Forms.RadioButton()
         Me.radioButtonHard = New System.Windows.Forms.RadioButton()
         Me.newGameButton = New System.Windows.Forms.Button()
+        Me.minutesLabel = New System.Windows.Forms.Label()
+        Me.secondsLabel = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.timeLabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -45,7 +51,7 @@ Partial Class Form1
         Me.radioButtonEasy.AutoSize = True
         Me.radioButtonEasy.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
         Me.radioButtonEasy.Location = New System.Drawing.Point(156, 97)
-        Me.radioButtonEasy.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.radioButtonEasy.Margin = New System.Windows.Forms.Padding(4)
         Me.radioButtonEasy.Name = "radioButtonEasy"
         Me.radioButtonEasy.Size = New System.Drawing.Size(74, 29)
         Me.radioButtonEasy.TabIndex = 1
@@ -58,7 +64,7 @@ Partial Class Form1
         Me.radioButtonIntermedian.AutoSize = True
         Me.radioButtonIntermedian.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
         Me.radioButtonIntermedian.Location = New System.Drawing.Point(128, 145)
-        Me.radioButtonIntermedian.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.radioButtonIntermedian.Margin = New System.Windows.Forms.Padding(4)
         Me.radioButtonIntermedian.Name = "radioButtonIntermedian"
         Me.radioButtonIntermedian.Size = New System.Drawing.Size(124, 29)
         Me.radioButtonIntermedian.TabIndex = 2
@@ -71,7 +77,7 @@ Partial Class Form1
         Me.radioButtonHard.AutoSize = True
         Me.radioButtonHard.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
         Me.radioButtonHard.Location = New System.Drawing.Point(153, 194)
-        Me.radioButtonHard.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.radioButtonHard.Margin = New System.Windows.Forms.Padding(4)
         Me.radioButtonHard.Name = "radioButtonHard"
         Me.radioButtonHard.Size = New System.Drawing.Size(78, 29)
         Me.radioButtonHard.TabIndex = 3
@@ -83,25 +89,73 @@ Partial Class Form1
         '
         Me.newGameButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
         Me.newGameButton.Location = New System.Drawing.Point(121, 244)
-        Me.newGameButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.newGameButton.Margin = New System.Windows.Forms.Padding(4)
         Me.newGameButton.Name = "newGameButton"
         Me.newGameButton.Size = New System.Drawing.Size(149, 39)
         Me.newGameButton.TabIndex = 4
         Me.newGameButton.Text = "Nuevo Juego"
         Me.newGameButton.UseVisualStyleBackColor = True
         '
+        'minutesLabel
+        '
+        Me.minutesLabel.AutoSize = True
+        Me.minutesLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.minutesLabel.Location = New System.Drawing.Point(149, 721)
+        Me.minutesLabel.Name = "minutesLabel"
+        Me.minutesLabel.Size = New System.Drawing.Size(23, 25)
+        Me.minutesLabel.TabIndex = 5
+        Me.minutesLabel.Text = "0"
+        '
+        'secondsLabel
+        '
+        Me.secondsLabel.AutoSize = True
+        Me.secondsLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.secondsLabel.Location = New System.Drawing.Point(203, 721)
+        Me.secondsLabel.Name = "secondsLabel"
+        Me.secondsLabel.Size = New System.Drawing.Size(34, 25)
+        Me.secondsLabel.TabIndex = 6
+        Me.secondsLabel.Text = "00"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label2.Location = New System.Drawing.Point(178, 720)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(18, 25)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = ":"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'timeLabel
+        '
+        Me.timeLabel.AutoSize = True
+        Me.timeLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.timeLabel.Location = New System.Drawing.Point(143, 682)
+        Me.timeLabel.Name = "timeLabel"
+        Me.timeLabel.Size = New System.Drawing.Size(95, 25)
+        Me.timeLabel.TabIndex = 8
+        Me.timeLabel.Text = "TIEMPO"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.BackColor = System.Drawing.SystemColors.Control
         Me.ClientSize = New System.Drawing.Size(1512, 1055)
+        Me.Controls.Add(Me.timeLabel)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.secondsLabel)
+        Me.Controls.Add(Me.minutesLabel)
         Me.Controls.Add(Me.newGameButton)
         Me.Controls.Add(Me.radioButtonHard)
         Me.Controls.Add(Me.radioButtonIntermedian)
         Me.Controls.Add(Me.radioButtonEasy)
         Me.Controls.Add(Me.Label1)
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Form1"
         Me.Text = "Juego de Pares"
         Me.ResumeLayout(False)
@@ -114,4 +168,9 @@ Partial Class Form1
 	Friend WithEvents radioButtonIntermedian As RadioButton
 	Friend WithEvents radioButtonHard As RadioButton
 	Friend WithEvents newGameButton As Button
+    Friend WithEvents minutesLabel As Label
+    Friend WithEvents secondsLabel As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents timeLabel As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,6 +1,12 @@
 ﻿Public Class Form1
 	Dim game As New Juego
-
+	Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+		secondsLabel.Text += 1
+		If secondsLabel.Text = "60" Then
+			minutesLabel.Text = "1"
+			secondsLabel.Text = "00"
+		End If
+	End Sub
 	Public Sub unCheckRadioButtons()
 		radioButtonEasy.Checked = False
 		radioButtonIntermedian.Checked = False
@@ -14,6 +20,8 @@
 	End Sub
 	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 		newGameButton.Enabled = False
+		radioButtonEasy.TabStop = False
+		radioButtonIntermedian.TabStop = False
 		radioButtonHard.TabStop = False
 		radioButtonEasy.TabIndex = 0
 		radioButtonIntermedian.TabIndex = 0
@@ -23,6 +31,7 @@
 
 	Private Sub radioButtonEasy_CheckedChanged(sender As Object, e As EventArgs) Handles radioButtonEasy.CheckedChanged
 		game.generateBoard(1)
+		Timer1.Start()
 		radioButtonIntermedian.Enabled = False
 		radioButtonHard.Enabled = False
 		newGameButton.Enabled = True
@@ -49,6 +58,9 @@
 		unCheckRadioButtons()
 		enableRadioButtons()
 		game.eraseBoard()
+		Timer1.Stop()
+		secondsLabel.Text = "00"
+		minutesLabel.Text = 0
 	End Sub
 
 End Class
